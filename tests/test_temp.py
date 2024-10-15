1a69a2b0fa65e97fb25ce1c979123e38e43b3e64
sinnovah <sinnovah@gmail.com>
2024-10-16 06:44:43 +1300

    docstring linting is working in deploy file - format test file correctly

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -6,6 +6,6 @@ class TemporaryTest(unittest.TestCase):
     """Temporary uniting class that the deployment workflow can find."""
 
     def test_example(self):
-
+        """Temporary unit test method that the deployment workflow can find."""
         # 1 + 1 is equal to 2, test will pass
         self.assertEqual(1 + 1, 2)
