edb3da8d3b41baf8a8419642a81220a8033f880c
sinnovah <sinnovah@gmail.com>
2024-10-16 06:38:33 +1300

    add docstring extension to linter

--- README.md
+++ README.md
@@ -41,9 +41,12 @@
     - For production environment:
         ```
         pip install -r requirements.txt
+        ```
+
+## Linting
+To run the linter, run the following terminal command:
+    ```flake8```
 
 ## Unit testing
 
-To run the unit tests, run the following terminal command:
-    ```python -m unittest discover tests
-    ```
+To run the unit tests, run the following terminal command: ```python -m unittest discover tests```
--- tests/test_temp.py
+++ tests/test_temp.py
@@ -1,13 +1,11 @@
+"""Temporary uniting testing module that the deployment workflow can find."""
 import unittest
 
 
 class TemporaryTest(unittest.TestCase):
-    '''
-    A temporary test class to stop the CI/CD tests from failing
-    '''
+    """Temporary uniting class that the deployment workflow can find."""
 
     def test_example(self):
-        ''' Example unit test '''
 
         # 1 + 1 is equal to 2, test will pass
         self.assertEqual(1 + 1, 2)
