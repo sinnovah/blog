d4aa11eea22bcf93b93afa370fb2976c2dd6cbf9
sinnovah <sinnovah@gmail.com>
2024-10-16 06:48:38 +1300

    fix typos

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -1,11 +1,11 @@
-"""Temporary uniting testing module that the deployment workflow can find."""
+"""Temporary unit testing module that the deployment workflow can find."""
 import unittest
 
 
 class TemporaryTest(unittest.TestCase):
-    """Temporary uniting class that the deployment workflow can find."""
+    """Temporary unit testing class that the deployment workflow can find."""
 
     def test_example(self):
-        """Temporary unit test method that the deployment workflow can find."""
+        """Temporary unit testing method that the deployment workflow can find."""
         # 1 + 1 is equal to 2, test will pass
         self.assertEqual(1 + 1, 2)
