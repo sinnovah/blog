e519665ef165e6f1cbc65ba20794447450c40fbc
sinnovah <sinnovah@gmail.com>
2024-10-16 22:30:11 +1300

    refactor

--- app.py
+++ app.py
@@ -8,21 +8,27 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 load_dotenv()
 
 
+def get_html_content():
+    """Get the HTML content from the template."""
+    # Get the BLOG_NAME environment variable, default to "My Blog"
+    blog_name = os.getenv("BLOG_NAME", "My Blog")
+
+    # Read the HTML template from file
+    with open('template.html', 'r') as file:
+        html_template = file.read()
+
+    # Replace the placeholder with the actual blog name
+    html_content = html_template.replace('{{ blog_name }}', blog_name)
+
+    return html_content
+
+
 class BlogHTTPRequestHandler(BaseHTTPRequestHandler):
     """Handler to serve the page."""
 
     def do_GET(self):
         """Serve the HTML content."""
-        # Get the BLOG_NAME environment variable, default to "My Blog"
-        blog_name = os.getenv("BLOG_NAME", "My Blog")
-
-        # Read the HTML template from file
-        with open('template.html', 'r') as file:
-            html_template = file.read()
-
-        # Replace the placeholder with the actual blog name
-        html_content = html_template.replace('{{ blog_name }}', blog_name)
-
+        html_content = get_html_content()
         # Send HTTP response
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -31,33 +37,31 @@ class BlogHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def run_development_server(
-    server_class=HTTPServer,
-    handler_class=BlogHTTPRequestHandler
+    server_class=HTTPServer, handler_class=BlogHTTPRequestHandler
 ):
-    """Run the server indefinitely."""
-    # Port number from the environment variable, or default to 8000
+    """Run the development server."""
     port = int(os.getenv("SERVER_PORT", 8000))
-
-    # '' means all available interfaces
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}...")
+    print(f"Starting development server on port {port}...")
 
-    # Open the browser automatically
     webbrowser.open(f'http://localhost:{port}')
     httpd.serve_forever()
 
 
-def run_app():
-    """
-    Run the application.
+def run_app(environ, start_response):
+    """WSGI application callable for production."""
+    # Prepare HTML content
+    html_content = get_html_content()
 
-    This method need to stay here so that wsgi
-    has something to call in production.
-    """
-    if os.getenv("ENVIRONMENT") == "Development":
-        run_development_server()
+    # WSGI response headers and body
+    status = '200 OK'
+    headers = [('Content-type', 'text/html; charset=utf-8')]
+    start_response(status, headers)
+
+    return [html_content.encode('utf-8')]
 
 
 if __name__ == "__main__":
-    run_app()
+    if os.getenv("ENVIRONMENT") == "Development":
+        run_development_server()
