4075d9643b95813095b6e5a4d1b83e9bb3638055
sinnovah <sinnovah@gmail.com>
2024-10-16 20:06:51 +1300

    use passenger config

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,16 @@
 """
-Module defining the  callable function.
+Module defining the wsgi callable function.
 
 Do not add any environment variables here because this file is included in
 source control. Use .env instead.
 """
 from app import run_app
+import imp
+import os
+import sys
 
 
-# Set the WSGI callable to the run_app function
+sys.path.insert(0, os.path.dirname(__file__))
+
+wsgi = imp.load_source('wsgi', 'app.py')
 application = run_app
