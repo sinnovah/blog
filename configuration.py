aa1f4bae42a3770fa196e1ad22e41facaa8be754
sinnovah <sinnovah@gmail.com>
2024-10-17 15:16:13 +1300

    comment code

--- configuration.py
+++ configuration.py
@@ -2,7 +2,12 @@ import os
 
 
 class Configuration:
+    """Class for site configuration."""
+
     def __init__(self):
+        """
+        Set site configuration values from .env file.
 
+        Adds default value if there is no environment variable specified.
+        """
         self.__blog_name = os.getenv("BLOG_NAME", "My Blog")
-
